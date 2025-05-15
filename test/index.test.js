032b1bbbcb81e53dee04132594fc3b324d1bf07b
nikitaks97 <nikitaks997@gmail.com>
2025-05-15 18:42:15 +0200

    updated

--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ describe('HTTP Server', function() {
     // Import the app and start the server only if not already started
     const app = require('../index');
     if (app && typeof app.listen === 'function') {
-      server = app.listen(3001, '127.0.0.1', done); // Use a different port for testing
+      server = app.listen(3002, '127.0.0.1', done); // Use a different port for testing
     } else if (app && typeof app.close === 'function') {
       server = app;
       setTimeout(done, 500);
@@ -23,7 +23,7 @@ describe('HTTP Server', function() {
     }
   });
   it('should return the homepage', function(done) {
-    http.get('http://127.0.0.1:3001/', (res) => {
+    http.get('http://127.0.0.1:3002/', (res) => {
       let data = '';
       res.on('data', chunk => data += chunk);
       res.on('end', () => {
@@ -33,7 +33,7 @@ describe('HTTP Server', function() {
     });
   });
   it('should return 404 for unknown page', function(done) {
-    http.get('http://127.0.0.1:3001/unknown', (res) => {
+    http.get('http://127.0.0.1:3002/unknown', (res) => {
       let data = '';
       res.on('data', chunk => data += chunk);
       res.on('end', () => {
