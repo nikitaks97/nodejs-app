9ef417f8376193b4c6ea8dea36d3582c5dcb006c
nikitaks97 <nikitaks997@gmail.com>
2025-05-15 18:40:23 +0200

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3002;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/' || req.url === '/index.html') {
